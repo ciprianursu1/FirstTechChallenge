f4babee5d025c3fb87d9903b1bf9500d2226624f
Ciprian Ursu <ursuciprian210@gmail.com>
2026-07-22 19:13:46 +0300

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/ExampleTeleOp.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@TeleOp(name = "ExampleTeleOp", group = "ZZZZZZZ")
+public class ExampleTeleOp extends OpMode {
+    private Follower follower;
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+    }
+    @Override
+    public void start() {
+        follower.startTeleopDrive();
+    }
+    @Override
+    public void loop() {
+        follower.setTeleOpDrive(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                -gamepad1.right_stick_x
+        );
+        telemetry.addData("Pose",follower.getPose().toString());
+        follower.update();
+    }
+}
