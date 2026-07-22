7412ae77bbaa67b09108acb2afb1a1c733d834e5
Ciprian Ursu <ursuciprian210@gmail.com>
2026-07-22 19:36:21 +0300

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,18 +1,108 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.control.FilteredPIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(15)
+            .forwardZeroPowerAcceleration(-35)
+            .lateralZeroPowerAcceleration(-45)
+            .translationalPIDFCoefficients(new PIDFCoefficients(
+                    0.09,
+                    0,
+                    0.00005,
+                    0.005
+            ))
+            .translationalPIDFSwitch(20)
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(
+                    0.06,
+                    0,
+                    0.0025,
+                    0.001
+            ))
+            .headingPIDFCoefficients(new PIDFCoefficients(
+                    1.1,
+                    0,
+                    0,
+                    0.01
+            ))
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(
+                    2.8,
+                    0,
+                    0.1,
+                    0.0005
+            ))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(
+                    0.1,
+                    0,
+                    0.00035,
+                    0.6,
+                    0.015
+            ))
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(
+                    0.02,
+                    0,
+                    0.000005,
+                    0.6,
+                    0.01
+            ))
+            .drivePIDFSwitch(15)
+            .centripetalScaling(0.0005);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName("lf")
+            .leftRearMotorName("lr")
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(61.722)
+            .yVelocity(57.464);
 
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-7.1356)
+            .strafePodX(-2.697)
+            .distanceUnit(DistanceUnit.INCH)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+    /**
+     These are the PathConstraints in order:
+     tValueConstraint, velocityConstraint, translationalConstraint, headingConstraint, timeoutConstraint,
+     brakingStrength, BEZIER_CURVE_SEARCH_LIMIT, brakingStart
+
+     The BEZIER_CURVE_SEARCH_LIMIT should typically be left at 10 and shouldn't be changed.
+     */
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.995,
+            0.1,
+            0.1,
+            0.009,
+            50,
+            1.25,
+            10,
+            1
+    );
+
+    //Add custom localizers or drivetrains here
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
